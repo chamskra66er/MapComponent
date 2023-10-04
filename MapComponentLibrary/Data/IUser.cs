b7637ee9f065bf639d6def3cbebbb1d680d117c1
Anatoliy Novikov <chamskra66er@gmail.com>
2023-10-04 22:50:27 +0300

    add class library component

---
+++ MapComponentLibrary/Data/IUser.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MapComponentLibrary.Data
+{
+    public interface IUser
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
